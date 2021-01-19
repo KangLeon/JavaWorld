87cf3bbde16539a16f422b3aa2bb2b3807f3cc25
Leon Kang <kangleonjy@gmail.com>
2021-01-20 07:21:46 +0800

    new
    new

--- DynamicArray/src/ArrayList.java
+++ DynamicArray/src/ArrayList.java
@@ -1,3 +1,48 @@
  public class ArrayList {
+    //清除所有元素
+     public void clear() {
 
+     }
+
+     //元素的数量
+     public int size() {
+        return 0;
+     }
+
+     //是否为空
+     public boolean isEmpty() {
+         return false;
+     }
+
+     //是否包含某个元素
+     public boolean contains(int element) {
+         return false;
+     }
+
+     //添加元素到尾部
+     public void add(int element) {
+
+     }
+
+     //获取index位置的元素
+     public int get(int index){
+         return 0;
+     }
+
+     //在index位置插入一个元素
+     public void add(int index,int element){
+
+     }
+
+     /*
+     * 删除index位置的元素
+     * */
+     public int remove(int index){
+         return 0;
+     }
+
+     //查看元素的索引
+     public int indexOf(int element) {//
+         return -1;
+     }
 }
--- DynamicArray/src/Main.java
+++ DynamicArray/src/Main.java
@@ -1,8 +1,13 @@
+
+
 public class Main {
 
     public static void main(String[] args) {
         int array[] = new int[] {11,22,33};
 
-
+        ArrayList list = new ArrayList();
+        list.add(11);
+        list.add(0,22);
+        list.remove(1);
     }
 }
