babada58ab8751811449de11fc64180a9a2894b1
Leon Kang <kangleonjy@gmail.com>
2021-01-31 13:12:30 +0800

    new
    new

--- DynamicArray/src/Main.java
+++ DynamicArray/src/Main.java
@@ -3,11 +3,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        int array[] = new int[] {11,22,33};
 
-        ArrayList list = new ArrayList();
-        list.add(11);
-        list.add(0,22);
-        list.remove(1);
     }
 }
